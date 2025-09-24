1d9b7474fccd9196f1f524fd378a5e26f26f28ae
RayRaf <rayraf@mail.ru>
2025-09-24 11:39:46 +0500

    Константы вынесены в .env

--- kipmoney/settings.py
+++ kipmoney/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmdzasteh_y6^6u*%ois#cf!g8pqx!a+3e_cl-3_cn+-(bm=je'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-mmdzasteh_y6^6u*%ois#cf!g8pqx!a+3e_cl-3_cn+-(bm=je')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['variously-growing-fennec.cloudpub.ru', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -124,18 +128,12 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "https://variously-growing-fennec.cloudpub.ru",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:8000,http://127.0.0.1:8000').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF settings
-CSRF_TRUSTED_ORIGINS = [
-    "https://variously-growing-fennec.cloudpub.ru",
-]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'https://variously-growing-fennec.cloudpub.ru').split(',')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
