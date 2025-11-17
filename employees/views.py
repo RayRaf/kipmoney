18a2113e7e066ac4e1f9853229f47a3c6d12bc6c
RayRaf <rayraf@mail.ru>
2025-11-17 14:20:42 +0500

    Добавлена пагинация для истории транзакций на странице деталей сотрудника

--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from .models import Employee
 from .forms import TransactionForm, EmployeeForm
 
@@ -16,7 +17,26 @@ def employee_list(request):
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     transactions = employee.transactions.order_by('-date_time')
-    return render(request, 'employees/employee_detail.html', {'employee': employee, 'transactions': transactions})
+    
+    # Получение количества записей на страницу из параметра запроса
+    per_page = request.GET.get('per_page', '10')
+    try:
+        per_page = int(per_page)
+        if per_page not in [10, 50, 100]:
+            per_page = 10
+    except ValueError:
+        per_page = 10
+    
+    # Пагинация
+    paginator = Paginator(transactions, per_page)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'employees/employee_detail.html', {
+        'employee': employee, 
+        'page_obj': page_obj,
+        'per_page': per_page
+    })
 
 @login_required
 def create_transaction(request):
